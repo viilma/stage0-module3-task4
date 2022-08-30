7fc3281e1c1bb56f195707a0c843bd96a73c6e44
Vilma Zaveckienė <v.zaveckiene@gmail.com>
2022-08-30 20:28:40 +0300

    stage0-module3-task4 done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+        float formula = (9 * a * a - 5*b + 2 + a - 7) * ((a + b- 4 * a *b ) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] word = {'c','h','a','r'};
+        for (int i = 0; i < word.length; i++) {
+            System.out.print(word[i]);
+        }
     }
 }
